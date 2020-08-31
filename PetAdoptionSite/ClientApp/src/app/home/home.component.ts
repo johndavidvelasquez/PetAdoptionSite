96899e580daeaa324e698aab629f8c0410daf209
Kyle Valderrama <valderrama.kyle@gmail.com>
2020-08-31 17:16:34 +0800

    Changes Home, Added Cards on Home

--- PetAdoptionSite/ClientApp/src/app/home/home.component.ts
+++ PetAdoptionSite/ClientApp/src/app/home/home.component.ts
@@ -3,6 +3,31 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  styles:[`
+    .example-header-image {
+      background-image: url('https://material.angular.io/assets/img/examples/shiba1.jpg');
+      background-size: cover;
+    }
+    .example-card
+    {
+      margin:5px;
+    }
+    .header
+    {
+      background: #1A6900;
+      width:100%;
+      color:white;
+      padding:20px;
+      padding-bottom:1px;
+    }
+    .content
+    {
+      padding:0;
+      border: 1px solid black;
+      width:100em;
+      margin:0;
+    }
+  `]
 })
 export class HomeComponent {
 }
