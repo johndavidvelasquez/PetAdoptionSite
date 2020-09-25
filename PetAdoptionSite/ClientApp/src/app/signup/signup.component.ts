1182a12f31569ded3e8df6dadd611d3981582ce1
johndavidvelasquez <johndavidvelaquezz@gmail.com>
2020-09-25 17:34:45 +0800

    minor fixes - signup

--- PetAdoptionSite/ClientApp/src/app/signup/signup.component.ts
+++ PetAdoptionSite/ClientApp/src/app/signup/signup.component.ts
@@ -20,26 +20,40 @@ export class SignupComponent implements OnInit {
 
   ngOnInit() {
 
+    this.createForm();
+
     this._commonService.getProvinces()
     .subscribe(data => this.provinces = data);
 
     this._commonService.getCities()
     .subscribe(data => this.cities = data);
 
-    this.createForm();
 
   }
 
   createForm() {
     this.formGroup = this.formBuilder.group({
       'firstName': [null, Validators.required],
-      'lastName': [null, Validators.required]
+      'lastName': [null, Validators.required],
+      'contactNumber': [null, Validators.required],
+      'province': [null, Validators.required],
+      'city': [null, Validators.required],
+      'userName': [null, Validators.required],
+      'password': [null, Validators.required]
     });
   }
 
   selectProvince(value:string) {
     this.citiesData = this.cities.filter(item => item.province === value);
-    console.log(this.citiesData);
+  }
+
+  onSubmit(post) {
+    // this.petpostService.postPetPosts(post).subscribe(result => {
+    //   //Insert dialog boxes
+    //   this.router.navigateByUrl('/');
+    // }, error => console.error(error));
+    console.log(post);
+
   }
 
 }
